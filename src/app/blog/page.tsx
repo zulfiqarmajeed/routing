33843db85433f5b747f958dc02187f7fc1abc0c6
zulfiqar majeed <zulfiqarmajeed66@gmail.com>
2024-03-29 06:44:23 +0500

    routings

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function blog () {
+  return (
+    <>
+      <div>This is Blog page.</div>
+
+    </>
+  )
+}
+
+export default blog;
---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function Contactus () {
+  return (
+    <>
+      <div>This is Contact-us page.</div>
+
+    </>
+  )
+}
+
+export default Contactus;
